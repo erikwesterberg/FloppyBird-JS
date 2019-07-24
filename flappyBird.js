f5eafc89f26afc05d18dd20127ca71cef83a9e8a
erka <erik.westerberg@hotmail.se>
2019-07-24 15:59:25 +0200

    saved all pictures in variables and start drawing background to canvas

--- flappyBird.js
+++ flappyBird.js
@@ -1,6 +1,30 @@
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
 
+// load images
 
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeNorth = new Image();
+let pipeSouth = new Image();
 
+bird.src = "images/bird.png"
+bg.src = "images/bg.png"
+fg.src = "images/fg.png"
+pipeNorth.src = "images/pipeNorth.png"
+pipeSouth.src = "images/pipeSouth.png"
+
+console.log(bird)
+
+const draw = () => {
+    ctx.drawImage(bg,0,0);
+
+    // ctx.drawImage(pipeNorth,100,0);
+    // ctx.drawImage(pipeSouth,100,0+constant)
+}
+ 
+draw()
 
 
 
